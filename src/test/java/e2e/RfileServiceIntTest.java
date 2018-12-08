6ebdb5598024b1efa7e66e00dc97a22f56187926
CarlosVRL <carlos.varela.eng@gmail.com>
2018-12-08 00:11:55 -0600

    Add test to validate

---
+++ src/test/java/e2e/RfileServiceIntTest.java
@@ -0,0 +1,43 @@
+package e2e;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import rfile.domain.RfileResource;
+import rfile.service.RfileService;
+import rfile.service.impl.RfileSardineImpl;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.testng.Assert.*;
+
+/**
+ * E2E Integration Tests for RfileService.
+ */
+public class RfileServiceIntTest
+{
+
+    private RfileService rfileService;
+
+    private static final String ROOT_DIR = "/";
+
+    @BeforeMethod
+    private void init_test()
+    {
+        rfileService = new RfileSardineImpl().init();
+    }
+
+    @Test
+    public void can_init()
+    {
+        assertNotNull(rfileService);
+    }
+
+    @Test
+    public void can_list() throws IOException
+    {
+        List<RfileResource> rfileResources
+                = rfileService.list("http://192.168.99.100:8080/");
+        assertEquals(rfileResources.get(0).getPath(), ROOT_DIR);
+    }
+}
